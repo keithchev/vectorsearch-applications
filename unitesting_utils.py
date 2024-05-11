9768738cf989cf90ba8b9c531f17f02a00b5e0e9
Keith Cheveralls <keith.chev@gmail.com>
2024-05-11 09:05:22 -0700

    week one project complete

--- unitesting_utils.py
+++ unitesting_utils.py
@@ -2,30 +2,33 @@ import json
 import urllib.request
 import os
 
-def load_podcast_data(dataset_name: str='huberman_labs.json') -> list[dict]:
-    '''
+
+def load_podcast_data(dataset_name: str = "huberman_labs.json") -> list[dict]:
+    """
     Loads dataset in json format by trying three options:
     1. Assumes user is in Google Colab environment and loads file from content dir.
     2. If 1st option doesn't work, assumes user is in course notebooks repo and loads from data dir.
     3. If 2nd option doesn't work, assumes user does not have direct access to data so
        downloads data direct from Public course repo.
-    '''
+    """
     try:
-        path = f'/content/{dataset_name}'
+        path = f"/content/{dataset_name}"
         with open(path) as f:
             data = json.load(f)
         return data
     except Exception:
         print(f"Data not available at {path}")
-        try: 
+        try:
             path = f"{os.path.join(os.path.abspath('../'), f'data/{dataset_name}')}"
             with open(path) as f:
                 data = json.load(f)
             return data
         except Exception:
-            print(f'Data not available at {path}, downloading from source')
+            print(f"Data not available at {path}, downloading from source")
             try:
-                with urllib.request.urlopen(f"https://raw.githubusercontent.com/americanthinker/vector_search_applications_PUBLIC/master/{dataset_name}") as url:
+                with urllib.request.urlopen(
+                    f"https://raw.githubusercontent.com/americanthinker/vector_search_applications_PUBLIC/master/{dataset_name}"
+                ) as url:
                     data = json.load(url)
                 return data
             except Exception:
